53558072a22040beae5828f3f020c75d09dba521
Shivv9761 <shivv9761@gmail.com>
2023-05-09 19:48:26 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -5,24 +5,36 @@ const userRoutes = require("./routes/user");
 const app = express();
 var cors = require("cors");
 const PORT = process.env.PORT || 4000;
+const path = require("path");
 
 app.use(
   cors({
     origin: "*",
   })
 );
+app.use(express.static('public'));
+
 // Middleware
 app.use(express.json());
 
 app.use("/api/v1", userRoutes);
 
 // CORS Configuration
-app.listen(PORT, () => {
-  console.log(`THE SERVER IS UP AND RUNNING AT PORT ${PORT}`);
-});
 
 dbConnect();
 
-app.get("/", (req, res) => {
-  res.send(`<h1>Backend is Running and this is '/' Route</h1>`);
+// app.get("/", (req, res) => {
+//   res.send(`<h1>Backend is Running and this is '/' Route</h1>`);
+// });
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/frontend/build/index"));
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+);
+
+app.listen(PORT, () => {
+  console.log(`THE SERVER IS UP AND RUNNING AT PORT ${PORT}`);
 });
